ac499f033476640ed6efc7457b8eb007cf87c966
Intan2903 <150129971+Intan2903@users.noreply.github.com>
2025-08-14 14:17:13 +0700

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -11,19 +10,20 @@ class ExampleTest extends TestCase
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
-{
-    $response = $this->get('/');
+    {
+        $response = $this->get('/');
 
-    // Jika langsung OK
-    if ($response->status() === 200) {
-        $response->assertStatus(200);
-    } 
-    // Jika redirect
-    elseif ($response->status() === 302) {
-        $response->assertRedirect('/login'); // } 
-    else {
-        $this->fail('Unexpected status code: ' . $response->status());
+        // Jika langsung OK
+        if ($response->status() === 200) {
+            $response->assertStatus(200);
+        }
+        // Jika redirect
+        elseif ($response->status() === 302) {
+            $response->assertRedirect('/login'); 
+        }
+        // Jika status code tidak sesuai
+        else {
+            $this->fail('Unexpected status code: ' . $response->status());
+        }
     }
-}
-
 }
