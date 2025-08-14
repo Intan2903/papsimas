3c4e03278d7b89a6639f056b5c9c99e086cc20eb
Intan2903 <150129971+Intan2903@users.noreply.github.com>
2025-08-14 14:13:45 +0700

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -11,9 +11,19 @@ class ExampleTest extends TestCase
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
+{
+    $response = $this->get('/');
 
+    // Jika langsung OK
+    if ($response->status() === 200) {
         $response->assertStatus(200);
+    } 
+    // Jika redirect
+    elseif ($response->status() === 302) {
+        $response->assertRedirect('/login'); // } 
+    else {
+        $this->fail('Unexpected status code: ' . $response->status());
     }
+}
+
 }
